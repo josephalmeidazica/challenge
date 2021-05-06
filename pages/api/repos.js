581bf5fd454eb47b908d33d8c5578571d40fd340
josephalmeidazica <josephalmeidazica@gmail.com>
2021-05-05 23:41:53 -0300

    rota de busca dos repositorios

---
+++ pages/api/repos.js
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default (req, res) => {
+  let githubres = await fetch("https://api.github.com/orgs/takenet/repos?q=language:c#&limit:5&order:asc");
+  res.statusCode = 200
+  res.json({ githubres })
+}
+
+
