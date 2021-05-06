4eb6a88682b9b34ec151ebcdc05247a51d406217
josephalmeidazica <josephalmeidazica@gmail.com>
2021-05-05 23:42:59 -0300

    correção fetch

--- pages/api/repos.js
+++ pages/api/repos.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  let githubres = await fetch("https://api.github.com/orgs/takenet/repos?q=language:c#&limit:5&order:asc");
+  let githubres = fetch("https://api.github.com/orgs/takenet/repos?q=language:c#&limit:5&order:asc");
   res.statusCode = 200
   res.json({ githubres })
 }
