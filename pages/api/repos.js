7ce4fb748728261b6fb367aeeb3790a7cef9301c
josephalmeidazica <josephalmeidazica@gmail.com>
2021-05-06 12:48:14 -0300

    formatacao json carrossel

--- pages/api/repos.js
+++ pages/api/repos.js
@@ -1,14 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  let content = {};
-  content.push({itemtype: "application/vnd.lime.document-select+json"})
-  let items = []
+  let content = {
+    itemtype: "application/vnd.lime.document-select+json",
+    items: []
+  };
   fetch("https://api.github.com/orgs/takenet/repos?q=language:C#&limit:5&order:asc")
   .then(response => response.json())
   .then(json => {
-    json.map(el => { items.push( {name:el.name, descricao:el.description}) })
-    content.push(items);
+    json.map(dado => { content.items.push( {
+      header:{
+        type: "application/vnd.lime.media-link+json",
+        value: {
+            title: dado.name,
+            text: dado.description,
+            type: "image/jpeg",
+            uri: dado.owner.avatar_url,
+            aspectRatio: "1:1",
+        },
+      },
+    })
+  })
     res.statusCode = 200
     res.json({ content })
     });
