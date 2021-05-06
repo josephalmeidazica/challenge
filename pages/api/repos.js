054f3de9d26824407594761901a1cea7af85511d
josephalmeidazica <josephalmeidazica@gmail.com>
2021-05-06 12:25:25 -0300

    busca git api

--- pages/api/repos.js
+++ pages/api/repos.js
@@ -1,9 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  let githubres = fetch("https://api.github.com/orgs/takenet/repos?q=language:c#&limit:5&order:asc");
-  res.statusCode = 200
-  res.json({ githubres })
+  let dados = [];
+  fetch("https://api.github.com/orgs/takenet/repos?q=language:C#&limit:5&order:asc")
+  .then(response => response.json())
+  .then(json => {
+    json.map(el => { dados.push( {name:el.name, descricao:el.description}) })
+    res.statusCode = 200
+    res.json({ dados })
+    });
 }
 
 
