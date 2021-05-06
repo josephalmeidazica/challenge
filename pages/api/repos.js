55e06283eacbbb1d0c1c7a67b4b4038a7ed93734
josephalmeidazica <josephalmeidazica@gmail.com>
2021-05-06 12:35:22 -0300

    atualização resposta json

--- pages/api/repos.js
+++ pages/api/repos.js
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  let dados = [];
+  let content = {};
+  content.push({itemtype: "application/vnd.lime.document-select+json"})
+  let items = []
   fetch("https://api.github.com/orgs/takenet/repos?q=language:C#&limit:5&order:asc")
   .then(response => response.json())
   .then(json => {
-    json.map(el => { dados.push( {name:el.name, descricao:el.description}) })
+    json.map(el => { items.push( {name:el.name, descricao:el.description}) })
+    content.push(items);
     res.statusCode = 200
-    res.json({ dados })
+    res.json({ content })
     });
 }
 
